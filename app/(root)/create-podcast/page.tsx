c1a22006940acc2a6228e51bc4607e705b8073c4
Dude <dudeblogger1002gmail.com>
2024-07-01 16:16:54 +0530

    Basic Homepage Layout

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CreatePodcast = () => {
   return (
     <div>
-      <h1>CreatePodcast</h1>
+      <h1 className="text-white-1 font-bold text-4xl">CreatePodcast</h1>
     </div>
   );
 };
---
+++ components/MobileNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MobileNav = () => {
+  return (
+    <div>
+      <h1 className="text-white-1 text-5xl font-bold">Mobile Nav</h1>
+    </div>
+  );
+};
+
+export default MobileNav;
