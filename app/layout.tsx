0c079eb29f4641b8775d8c26c8a2babd05f20c64
Dude <dudeblogger1002gmail.com>
2024-07-01 16:55:08 +0530

    Convex Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import ConvexClientProvider from "./providers/ConvexClientProvider";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
