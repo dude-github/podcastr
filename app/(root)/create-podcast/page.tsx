8d6219807c1d0dc884814151342e674d54f36c73
Dude <dudeblogger1002gmail.com>
2024-07-01 14:41:59 +0530

    File & Folder Structure

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePodcast = () => {
+  return (
+    <div>
+      <h1>CreatePodcast</h1>
+    </div>
+  );
+};
+
+export default CreatePodcast;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <main>
+        <p className="text-white-1">LEFT</p>
+        {children}
+        <p className="text-white-1">RIGHT</p>
+      </main>
+    </div>
+  );
+}
